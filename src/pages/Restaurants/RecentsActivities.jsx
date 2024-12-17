8415a626c686633dc49586a4a50939e71d9c6a3d
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-12-17 10:39:52 +0600

    testimonial section start

--- src/pages/Restaurants/RecentsActivities.jsx
+++ src/pages/Restaurants/RecentsActivities.jsx
@@ -1,4 +1,5 @@
 import Container from "../../components/shared/Container/Container";
+import TestimonialSlider from "../../components/slides/TestimonialSlider";
 
 const RecentsActivities = () => {
   return (
@@ -8,6 +9,7 @@ const RecentsActivities = () => {
           <h2 className="font-extrabold text-d-color font-lexend text-xl lg:text-3xl mb-5">
           Recents avtivities
           </h2>
+          <TestimonialSlider />
         </div>
       </Container>
     </section>
